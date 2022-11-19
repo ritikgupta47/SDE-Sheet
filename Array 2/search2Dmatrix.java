b7e7d9663b80d77f16c6a029794a0d5ae369b368
ritik gupta <ritikguptacs47@gmail.com>
2022-11-19 12:40:09 +0530

    day2 updates

---
+++ Array 2/search2Dmatrix.java
@@ -0,0 +1,27 @@
+public class search2Dmatrix {
+    public static void main(String[] args) {
+        
+    }
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int lo = 0;
+        if(matrix.length == 0) return false;
+        int n = matrix.length; 
+        int m = matrix[0].length; 
+        int hi = (n * m) - 1;
+        
+        while(lo <= hi) {
+            int mid = (lo + (hi - lo) / 2);
+            if(matrix[mid/m][mid % m] == target) {
+                return true;
+            }
+            if(matrix[mid/m][mid % m] < target) {
+                lo = mid + 1;
+            }
+            else {
+                hi = mid - 1;
+            }
+        }
+        return false;
+    }
+
+}

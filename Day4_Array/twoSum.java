f25164de75c92f88bce5080565cf4bc3dd30b28f
ritik gupta <ritikguptacs47@gmail.com>
2022-11-19 12:43:29 +0530

    two sum

---
+++ Day4_Array/twoSum.java
@@ -0,0 +1,22 @@
+package Day4_Array;
+import java.util.*;
+public class twoSum {
+    public static void main(String[] args) {
+        
+    }
+    public int[] twoSumm(int[] nums, int target) {
+        int n=nums.length;
+        Map<Integer,Integer> map=new HashMap<>();
+        int[] result=new int[2];
+        for(int i=0;i<n;i++){
+            if(map.containsKey(target-nums[i])){
+                result[1]=i;
+                result[0]=map.get(target-nums[i]);
+                return result;
+            }
+            map.put(nums[i],i);
+        }
+        return result;
+    }
+
+}
---
+++ Day4_Array/twoSumTwo.java
@@ -0,0 +1,26 @@
+package Day4_Array;
+
+public class twoSumTwo {
+    public static void main(String[] args) {
+        
+    }
+    public int[] twoSum(int[] numbers, int target) {
+        int[] ans = new int[2];
+        int left = 0;
+        int right = numbers.length - 1;
+        if (numbers == null || numbers.length < 2) return ans;
+        while(left < right){
+            if(numbers[right] + numbers[left] == target){
+                ans[0] = left + 1;
+                ans[1] = right + 1;
+                break;
+            }else if(numbers[left] + numbers[right] > target){
+                right--;
+            }else if(numbers[left] + numbers[right] < target){
+                left++;
+            }
+        }
+        
+        return ans;
+    }
+}
